4af3343f44e33217438e3ac8b9ceb7d281c3a705
Patrick <pvilchez794@gmail.com>
2020-12-12 13:26:26 -0500

    Static files setting added

--- folder_organizer/settings.py
+++ folder_organizer/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.User'
